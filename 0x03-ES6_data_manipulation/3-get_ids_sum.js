90a9edac48d857d20aeacd5c27ed0ce43d868f0e
Ajiriwa Network <peterrobsson@gmail.com>
2023-10-06 09:21:14 +0300

    fixed eslint validatin for question 3

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,5 +1,5 @@
-function getStudentIdsSum(students){
-    return students.reduce((total, num) => total+num.id, 0);
+function getStudentIdsSum(students) {
+  return students.reduce((total, num) => total + num.id, 0);
 }
 
 module.exports = getStudentIdsSum;
