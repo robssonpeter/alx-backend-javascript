92e25a8bd290a4c2cfe786e280b811367a0b5e80
Peter Mgembe <peterrobsson@gmail.com>
2023-11-21 11:42:06 +0300

    tried to solve some questions

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,12 @@
+question = "Welcome to Holberton School, what is your name?";
+process.stdin.setEncoding('utf8');
+console.log(question);
+process.stdin.on('data', (name) => {
+    console.log(`Your name is: ${name}`);
+    process.exit();
+})
+
+process.on('exit', (code) => {
+    console.log(`This important software is now closing`)
+    process.exit()
+})
