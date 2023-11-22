011b78e466c68ca7955c161e9a090df534e31370
Peter Mgembe <peterrobsson@gmail.com>
2023-11-22 07:01:34 +0300

    added a unit test for question 0

---
+++ 0x05-Node_JS_basic/test/1_stdin.test.js
@@ -0,0 +1,7 @@
+const chai = require('chai');
+const expect = chai.expect;
+describe("Testing 1_stdin.js", () => {
+    it("testing the addition", () => {
+        expect(3+2).to.be.equal(5);
+    })
+})
