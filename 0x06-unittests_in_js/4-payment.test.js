eee29751ccf523f19ce20a52480a1c7d3f13beb5
Peter Mgembe <peterrobsson@gmail.com>
2023-11-23 14:11:11 +0300

    tried to fix question 4

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -2,22 +2,27 @@ const Utils = require('./utils.js')
 const sinon = require('sinon');
 const sendPaymentRequestToApi = require('./4-payment.js');
 
-const spy = sinon.spy(console, 'log');
-
 //const stub = sinon.stub(Utils, 'calculateNumber').yield(10);
 
-
-sendPaymentRequestToApi(100, 20);
 
 const total = Utils.calculateNumber('SUM', 100, 20)
 
 describe('Testing for the sendPayment Stubbed', () => {
-    before('calculating the total', () => {
-        sinon.stub(Utils, 'calculateNumber').returns(10);
+    let stub;
+    let spy;
+    before(() => {
+        stub = sinon.stub(Utils, 'calculateNumber').returns(10);
+
+        spy = sinon.spy(console, 'log');
     });
-    it(`#The total is: 10`,() => {
+    it(`#should log the correct total`,() => {
+        sendPaymentRequestToApi(100, 20);
+
         spy.calledWith(`The total is: ${total}`);
     })
-})
 
-spy.restore();
+    after(() => {
+        sinon.restore();
+        stub.restore();
+    })
+});
