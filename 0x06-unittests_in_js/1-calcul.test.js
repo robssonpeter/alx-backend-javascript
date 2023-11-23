933e79db4239cf91ee7e5b1f5c523ebab918bd84
Peter Mgembe <peterrobsson@gmail.com>
2023-11-23 12:19:03 +0300

    solving question 2

---
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,24 @@
+const calculateNumber = require('./1-calcul.js');
+const assert = require('assert');
+
+describe('checking the result for summation', () => {
+    it(`#calculateNumber('SUM', 4.3, 4.8) -> 9`, () => {
+        assert.equal(calculateNumber('SUM', 4.3, 4.8), 9);
+    });
+});
+
+describe('checking the result for subtraction', () => {
+    it(`#calculateNumber('SUBTRACT', 4.3, 4.8) -> -1`, () => {
+        assert.equal(calculateNumber('SUBTRACT', 4.3, 4.8), -1);
+    });
+});
+
+describe('Checking the result for division', () => {
+    it(`#calculateNumber('DIVIDE', 4.3, 4.8) == 0.8`, () => {
+        assert.equal(calculateNumber('DIVIDE', 4.3, 4.8), 0.8);
+    });
+
+    it(`#calculateNumber('DIVIDE', 4.3, 0.4) == Error`, () => {
+        assert.equal(calculateNumber('DIVIDE', 4.3, 0.4), 'Error');
+    })
+})
