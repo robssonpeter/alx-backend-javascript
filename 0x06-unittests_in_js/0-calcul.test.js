fab7092286573df03b26a51b51a69d7e56db151e
Peter Mgembe <peterrobsson@gmail.com>
2023-11-23 11:57:53 +0300

    provided solution for question number 1

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,7 +2,16 @@ const calculateNumber = require('./0-calcul.js');
 const assert = require('assert');
 
 describe('testing the summation', () => {
-    it('checking the result', () => {
-        assert.equal(calculateNumber(4.3, 4.8), 9);
-    })
-})
+    it('checking the result for summation', () => {
+        assert.equal(calculateNumber('SUM', 4.3, 4.8), 9);
+    });
+
+    it('checking the result for subtraction', () => {
+        assert.equal(calculateNumber('SUBTRACT', 4.3, 4.8), -1);
+    });
+    
+    it('Checking the result for division', () => {
+        assert.equal(calculateNumber('DIVIDE', 4.3, 4.8), 0.8);
+        assert.equal(calculateNumber('DIVIDE', 4.3, 0.4), 'Error');
+    });
+});
