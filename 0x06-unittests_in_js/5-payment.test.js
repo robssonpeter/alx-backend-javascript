27fb5b92bcafdfa294d0b86f7db05d2bbd646748
Peter Mgembe <peterrobsson@gmail.com>
2023-11-23 14:37:44 +0300

    concept of hooks

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,29 @@
+const sinon = require('sinon');
+const sendPaymentRequestToApi = require('./5-payment.js');
+const Utils = require('./utils.js');
+
+
+describe('Testing for the sendPayment Stubbed', () => {
+    let spy;
+    let total;
+    before(() => {
+        spy = sinon.spy(console, 'log');
+    });
+    it(`#should log the correct total 120`,() => {
+        total = Utils.calculateNumber('SUM', 100, 20);
+        sendPaymentRequestToApi(100, 20);
+
+        spy.calledOnceWith(`The total is: ${total}`);
+    })
+
+    it("#returns total 20", () => {
+        total = Utils.calculateNumber('SUM', 10, 10);
+        sendPaymentRequestToApi(10, 10);
+
+        spy.calledOnceWith(`The total is: ${total}`);
+    })
+
+    after(() => {
+        sinon.restore();
+    })
+});
