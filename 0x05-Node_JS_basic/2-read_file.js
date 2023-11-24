08050e98d46096b75537bfce9b4bea2bfe6d0d1d
Peter Mgembe <peterrobsson@gmail.com>
2023-11-24 15:34:39 +0300

    fixe the lint validation for questin 3

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,36 +1,33 @@
-const { error } = require('console');
-const fs = require('fs')
-const path = 'database.csv';
+const fs = require('fs');
 
-function countStudents(path){
-    try{
-        const file = fs.readFileSync(path, {encoding: 'utf-8'});
-        const rows = file.split('\n')
-        let fields = {};
-        let keys = [];
-        let row = [];
-        console.log(`Number of students: ${rows.length - 1}`)
-        rows.forEach(element => {
-            row = element.split(',');
-            keys = Object.keys(fields);
-            if (keys.indexOf(row[3]) < 0){
-                fields[row[3]] = {count: 1, students: [row[0]]};
-            }else{
-                fields[row[3]]['count'] = fields[row[3]]['count'] + 1;
-                fields[row[3]]['students'].push(row[0])
-            }
-        })
-        keys.forEach(key => {
-            let students = fields[key]['students'].join(', ');
-            let count = fields[key]['count'];
-            let msg = `Number of students in ${key}: ${count}. List: ${students}`
-            console.log(msg)
-        })
-        
-    }catch(exception){
-        //console.log(exception)
-        throw new Error('Cannot load the database');
-    }
+function countStudents(path) {
+  try {
+    const file = fs.readFileSync(path, { encoding: 'utf-8' });
+    const rows = file.split('\n');
+    const fields = {};
+    let keys = [];
+    let row = [];
+    console.log(`Number of students: ${rows.length - 1}`);
+    rows.forEach((element) => {
+      row = element.split(',');
+      keys = Object.keys(fields);
+      if (keys.indexOf(row[3]) < 0) {
+        fields[row[3]] = { count: 1, students: [row[0]] };
+      } else {
+        fields[row[3]].count += 1;
+        fields[row[3]].students.push(row[0]);
+      }
+    });
+    keys.forEach((key) => {
+      const students = fields[key].students.join(', ');
+      const { count } = fields[key];
+      const msg = `Number of students in ${key}: ${count}. List: ${students}`;
+      console.log(msg);
+    });
+  } catch (exception) {
+    // console.log(exception)
+    throw new Error('Cannot load the database');
+  }
 }
 
-module.exports = countStudents
+module.exports = countStudents;
